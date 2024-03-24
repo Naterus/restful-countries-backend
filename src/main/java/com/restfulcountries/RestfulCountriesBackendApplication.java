ed070a3434d2beeea33dbc2817c3cf80ea364c7e
Nathan Dauda <nathandauda27@gmail.com>
2024-03-24 23:25:37 +0000

    initial commit

---
+++ src/main/java/com/restfulcountries/RestfulCountriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.restfulcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCountriesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulCountriesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restfulcountries/RestfulCountriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCountriesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
